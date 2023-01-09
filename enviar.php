6de66c3df9486b53f5cf7ae3e6d382839a499f93
Jordan Toledo <Jordan-direito12@hotmail.com>
2023-01-08 21:19:36 -0500

    Finalizado Style Interface

--- enviar.php
+++ enviar.php
@@ -3,7 +3,7 @@
 $uploadfile = "xml/".md5(basename($_FILES['arquivo']['name']).time()).".xml";
 
 if (move_uploaded_file($_FILES['arquivo']['tmp_name'], $uploadfile)) {
-    echo "Arquivo válido e enviado com sucesso.\n";
+    echo "Arquivo válido e Registrado!\n";
     echo "<a href='lista.php'>Ir para lista de arquivos.</a>";
 } else {
     echo "Erro ao enviar arquivo!\n";
--- index.php
+++ index.php
@@ -1,20 +1,36 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" type="text/css" href="style.css">
     <title>Thanos</title>
 </head>
+
 <body>
     <section>
-        <form action="enviar.php" method="post" enctype="multipart/form-data">
-            <input type="file" name="arquivo" />
-            <br /><br />
-            <input type="submit" value="Enviar" />
+        <form action="enviar.php" autocomplete="off" method="post" enctype="multipart/form-data">
+            <p>Importar XML</p>
+            <div>
+                <input type="file" id="upload" style="display: none;" name="arquivo" />
+                <label for="upload">Selecionar XML</label>
+            </div>
+            <button><span>&#8682; Registrar XML </span><span class="uploading"> XML </span></button>
+            <button class="cancel">Cancelar</button>
+            <button><a href='lista.php' style="text-decoration: none">Arquivos</a></button>
+            <div class="pr">
+                <strong>
+                    <h4 class="ex">PDF</h4>
+                    <h5 class="size">2.5kb</h5>
+                </strong>
+                <progress min="0" max="100" value="0"></progress>
+                <span class="progress-indicator"></span>
+            </div>
         </form>
     </section>
-    <br />
-    <a href='lista.php'><h3>Lista de arquivos</h3></a><br />
+    
 </body>
+<script type="text/javascript" src="main.js"></script>
 </html>
